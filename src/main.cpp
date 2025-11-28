d59ad89a890c4fb7ec52a0e361f1436e91081421
Gurman <gurmankaur0603@gmail.com>
2025-11-28 11:34:45 +0530

    chore: add C++ project skeleton

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <set>
+#include <map>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    cout << "Regex to NFA to DFA Converter\n";
+    return 0;
+}
