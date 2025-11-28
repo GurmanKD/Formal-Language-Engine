80b6c6d1a20931b36f3b13ffe72da372653c4b83
Gurman <gurmankaur0603@gmail.com>
2025-11-28 11:35:45 +0530

    feat: define core automaton state and NFA segment structures

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,21 @@
 #include <iomanip>
 using namespace std;
 
+class AutomatonState {
+public:
+    vector<int> epsilonMoves;
+    map<char, vector<int>> symbolMoves;
+};
+
+class NFASegment {
+public:
+    int startState;
+    int endState;
+
+    NFASegment(int start = -1, int end = -1)
+        : startState(start), endState(end) {}
+};
+
 int main() {
     cout << "Regex to NFA to DFA Converter\n";
     return 0;
